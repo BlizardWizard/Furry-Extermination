a495aa5f7cef9dd32c97f74875a99ed74266a059
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2019-03-01 20:01:51 -0800

    testing with vision client

--- run.py
+++ run.py
@@ -14,9 +14,9 @@ from google.oauth2 import service_account
 # project_dir = os.getcwd().replace('\\', '/')
 # Change these values to the correct values for your files
 
-vision_client = vision.ImageAnnotatorClient(
-    credentials=service_account.Credentials.from_service_account_info(
-        json.loads(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))))
+# vision_client = vision.ImageAnnotatorClient(
+#     credentials=service_account.Credentials.from_service_account_info(
+#         json.loads(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))))
 
 discord_client = discord.Client()
 discord_bot = commands.Bot(command_prefix="%")
@@ -30,15 +30,15 @@ async def on_ready():
     await discord_bot.change_presence(game=discord.Game(name="Filtering Images"))
 
 
-    url = 'https://media1.tenor.com/images/59371e16bf2c92a158a0bf84e1e70bb6/tenor.gif'
-
-    image = types.Image()
-    image.source.image_uri = url
-
-
-    response = vision_client.label_detection(image=image)
-    labels = response.label_annotations
-    for label in labels:
-        await discord_bot.send_message('512428434560122913', label)
+    # url = 'https://media1.tenor.com/images/59371e16bf2c92a158a0bf84e1e70bb6/tenor.gif'
+    #
+    # image = types.Image()
+    # image.source.image_uri = url
+    #
+    #
+    # response = vision_client.label_detection(image=image)
+    # labels = response.label_annotations
+    # for label in labels:
+    #     await discord_bot.send_message('512428434560122913', label)
 
 discord_bot.run(os.environ.get('BOT_TOKEN'))
